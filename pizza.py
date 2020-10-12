01f5d575b2546de2b7ccb8a7e323addb8a6b5367
KongtappV <Kongtapp.v@ku.th>
2020-10-12 13:26:32 +0700

    refactoring code

--- pizza.py
+++ pizza.py
@@ -1,24 +1,40 @@
 class Pizza:
     """A pizza with a size and optional toppings."""
+    SMALL = 'small'
+    MEDIUM = 'medium'
+    LARGE = 'large'
 
     def __init__(self, size):
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
+        if self.size == self.SMALL:
             price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
+        elif self.size == self.MEDIUM:
             price = 200 + 25*len(self.toppings)
-        elif self.size == 'large':
+        elif self.size == self.LARGE:
             price = 280 + 30*len(self.toppings)
         else:
             raise ValueError("Unknown pizza size "+self.size)
         return price
-    
-    def addTopping(self, topping):
+
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
 
+    def print_pizza(self, pizza):
+        """
+        Print a description of a pizza, along with its price.
+        """
+        # create printable description of the pizza such as
+        # "small pizza with muschroom" or "small plain pizza"
+        description = pizza.size
+        if pizza.toppings:
+            description += " pizza with "+ ", ".join(pizza.toppings)
+        else:
+            description += " plain pizza"
+        print(f"A {description}")
+        print("Price:", pizza.get_price())
--- pizzashop.py
+++ pizzashop.py
@@ -8,31 +8,16 @@ from pizza import Pizza
 # - if no type annotation on the pizza parameter, maybe not
 # - if use type annotation ':Pizza' on the parameter, it should
 
-def print_pizza( pizza ):
-    """
-    Print a description of a pizza, along with its price.
-    """
-    # create printable description of the pizza such as
-    # "small pizza with muschroom" or "small plain pizza"
-    description = pizza.size
-    if pizza.toppings:
-        description += " pizza with "+ ", ".join(pizza.toppings)
-    else:
-        description += " plain pizza"
-    print(f"A {description}")
-    print("Price:", pizza.getPrice())
-
-
 if __name__ == "__main__":
-    pizza = Pizza('small')
-    pizza.addTopping("mushroom")
-    pizza.addTopping("tomato")
-    pizza.addTopping("pinapple")
-    print_pizza(pizza)
+    pizza = Pizza(Pizza.SMALL)
+    pizza.add_topping("mushroom")
+    pizza.add_topping("tomato")
+    pizza.add_topping("pinapple")
+    pizza.print_pizza(pizza)
 
-    pizza2 = Pizza("medium")
-    print_pizza(pizza2)
+    pizza2 = Pizza(Pizza.MEDIUM)
+    pizza.print_pizza(pizza2)
 
-    pizza3 = Pizza("large")
-    pizza3.addTopping("seafood")
-    print_pizza(pizza3)
+    pizza3 = Pizza(Pizza.LARGE)
+    pizza3.add_topping("seafood")
+    pizza.print_pizza(pizza3)
